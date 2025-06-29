14e731a2dfa83a788e59c930df4680d2f0b4107a
Ajay Nishad <ajaynishad4123@gmail.com>
2025-06-29 16:52:51 +0530

    add media query to detect mobile screen for navbar

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -8,40 +8,49 @@ import { MdMinimize } from "react-icons/md";
 import { PiCopyLight } from "react-icons/pi";
 import { VscSearch } from "react-icons/vsc";
 import { MdOutlineClose } from "react-icons/md";
+import useMediaQuery from "utils/useMediaQuery";
 
 function Navbar() {
+  const isMobile = useMediaQuery("(max-width: 768px)");
   return (
     <div className="div-flex-column navbar-theme-black grey-border-bottom">
       <div className="navbar-container-padding">
         <Row className="g-0 m-0 p-0">
-          <Col xl={4} lg={4} md={4} sm={4}>
-            <div className="div-flex-row  div-align-center cg-20 text-white">
-              <VscVscode className="vscode-icon icon-size-20" />
-              <div className="div-flex-row  div-align-center cg-15">
-                <span className="text-md">File</span>
-                <span className="text-md">Edit</span>
-                <span className="text-md">Selection</span>
-                <span className="text-md">View</span>
-                <span className="text-md">Go</span>
-                <span className="text-md">Run</span>
-                <span className="text-md">Terminal</span>
-                <span className="text-md">Help</span>
+          <Col xl={4} lg={4} md={4} sm={6} xs={6}>
+            <div className="div-flex-row  div-align-center cg-20 text-white ">
+              <div className="div-flex-row div-align-center py-1 cg-25">
+                <VscVscode className="vscode-icon icon-size-20 " />
               </div>
+              {!isMobile && (
+                <div className="div-flex-row  div-align-center cg-15">
+                  <span className="text-md">File</span>
+                  <span className="text-md">Edit</span>
+                  <span className="text-md">Selection</span>
+                  <span className="text-md">View</span>
+                  <span className="text-md">Go</span>
+                  <span className="text-md">Run</span>
+                  <span className="text-md">Terminal</span>
+                  <span className="text-md">Help</span>
+                </div>
+              )}
             </div>
           </Col>
-          <Col xl={4} lg={4} md={4} sm={4}>
-            <div className="div-flex-row cg-10 div-flex-center text-white">
-              <IoIosArrowRoundBack className="icon-size-24" />
-              <IoIosArrowRoundForward className="icon-size-24" />
-              <div className="div-flex-row div-align-center vscode-search">
-                <div className="div-flex-center div-align-center cg-5 ">
-                  <VscSearch className="icon-size-16" />
-                  <span className="text-sm"> vscode-portfolio</span>
+          {!isMobile && (
+            <Col xl={4} lg={4} md={4} sm={4}>
+              <div className="div-flex-row cg-10 div-flex-center text-white">
+                <IoIosArrowRoundBack className="icon-size-24" />
+                <IoIosArrowRoundForward className="icon-size-24" />
+                <div className="div-flex-row div-align-center vscode-search">
+                  <div className="div-flex-center div-align-center cg-5 ">
+                    <VscSearch className="icon-size-16" />
+                    <span className="text-sm"> vscode-portfolio</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Col>
-          <Col xl={4} lg={4} md={4} sm={4} className="div-align-center">
+            </Col>
+          )}
+
+          <Col xl={4} lg={4} md={4} sm={6} xs={6} className="div-align-center">
             <div className="div-flex-row div-align-center div-flex-end text-white">
               <div className="div-flex-row cg-25">
                 <MdMinimize />

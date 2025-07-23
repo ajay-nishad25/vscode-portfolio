55d8bb32cb1b5ac100e6345f6a262c1774797213
Ajay Nishad <ajaynishad4123@gmail.com>
2025-07-23 22:50:03 +0530

    add aboutTech.css code

--- src/components/EditorTabs/About.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function About() {
-  return (
-    <div className="hp-5">
-      <span className="text-sm">About.js</span>
-    </div>
-  );
-}
--- src/utils/CodeEditor.js
+++ src/utils/CodeEditor.js
@@ -2,11 +2,13 @@ import React from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 import { javascript } from "@codemirror/lang-javascript";
+import { css } from "@codemirror/lang-css"; // <-- Add this line
 import "styles/codeEditor.css";
 
-export default function CodeEditor({ value, height = "95vh", lang = "js" }) {
+export default function CodeEditor({ value, height = "95vh", lang }) {
   let extensions = [];
   if (lang === "js") extensions = [javascript()];
+  if (lang === "css") extensions = [css()];
 
   return (
     <CodeMirror

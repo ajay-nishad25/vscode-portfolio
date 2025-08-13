576b1e65d244cfe7c8a8ecd4a9065dc977037a7e
Ajay Nishad <ajaynishad4123@gmail.com>
2025-08-13 21:16:36 +0530

    wip

--- src/pages/File.js
+++ src/pages/File.js
@@ -189,7 +189,7 @@ export default function File() {
 
         <Col xl={10} lg={9} md={8} sm={7} xs={12}>
           <div className="div-flex-column">
-            <div className="div-flex-row file-tab-section-header file-tab-section-scroll div-align-center grey-border-bottom">
+            <div className="div-flex-row file-tab-section-header file-tab-section-scroll div-align-center ">
               {openTabs.map((file) => (
                 <div
                   key={file}
